2c727cf8e505ee158ee08940f55328ee6dbb7845
Amanda Viória <amandavic7576@gmail.com>
2023-04-21 14:01:53 -0300

    atores

--- pages/filmes/[id].js
+++ pages/filmes/[id].js
@@ -1,43 +1,47 @@
 import Pagina from '@/components/Pagina'
 import apiFilmes from '@/services/apiFilmes'
+import Link from 'next/link'
 import React from 'react'
 import { Card, Col, Row } from 'react-bootstrap'
 
 const Detalhes = ({ filme, atores }) => {
+
     return (
         <Pagina titulo={filme.title}>
 
             <Row>
                 <Col md={3}>
-                    <Card.Img variant="top" src={'https://image.tmdb.org/t/p/w500/' + filme.poster_path} />
+                    <Card.Img variant="top" src={"https://image.tmdb.org/t/p/w500" + filme.poster_path} />
                 </Col>
 
                 <Col md={9}>
-                    <p><strong>Data de Lançamento: </strong>{filme.release_date}</p>
                     <p><strong>Orçamento: </strong>{filme.budget}</p>
+                    <p><strong>Data de Lançamento: </strong>{filme.release_date}</p>
                     <p><strong>Duração: </strong>{filme.runtime} min</p>
                     <p><strong>Nota: </strong>{filme.vote_average}</p>
                     <div>
+                        <strong>Gêneros: </strong>
                         <ul>
                             {filme.genres.map(item => (
                                 <li>{item.name}</li>
                             ))}
                         </ul>
                     </div>
-                    <p><strong>Sinopse: </strong>{filme.overview}</p>
-
+                    <p>{filme.overview}</p>
                 </Col>
             </Row>
-            <br></br>
 
-            <h2>Atores</h2> 
+            <h2>Atores</h2>
             <Row>
                 {atores.map(item => (
-                    <Col className='mb-3' md={2}>
-                        <Card.Img  title={item.name} variant="top" src={'https://image.tmdb.org/t/p/w500/' + item.profile_path} />
+                    <Col className='mb-3' md={2} title={item.name}>
+                        <Link href={'/atores/' + item.id}>
+                            <Card.Img variant="top" src={"https://image.tmdb.org/t/p/w500" + item.profile_path} />
+                        </Link>
                     </Col>
                 ))}
             </Row>
+
 
         </Pagina>
     )
@@ -56,6 +60,6 @@ export async function getServerSideProps(context) {
     const atores = resAtores.data.cast
 
     return {
-        props: { filme, atores }
+        props: { filme, atores },
     }
 }
